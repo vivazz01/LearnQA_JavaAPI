b7c43c3d9ce6f1553101d0f2a430246dc76e3b7a
vivazz01 <crashdemo06@gmail.com>
2024-07-25 12:25:32 +0300

    Ex4: GET-запрос

---
+++ src/test/java/Ex4GetRequest.java
@@ -0,0 +1,15 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Ex4GetRequest {
+
+    @Test
+    public void ex4GetRequestTest() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+
+}
