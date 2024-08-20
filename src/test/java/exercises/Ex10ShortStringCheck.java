5ea055999344a0821120b0d06c0c2756670d3e5f
vivazz01 <crashdemo06@gmail.com>
2024-08-20 10:23:04 +0300

    ex10

---
+++ src/test/java/exercises/Ex10ShortStringCheck.java
@@ -0,0 +1,22 @@
+package exercises;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import lib.BaseTestCase;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Ex10ShortStringCheck extends BaseTestCase {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0123456789", "0123456789ABCDE", "0123456789ABCDEFG"})
+    public void ex10ShortStringCheck(String name) {
+        assertTrue(name.length() >= 15, "String length is lesser than 15");
+    }
+}
