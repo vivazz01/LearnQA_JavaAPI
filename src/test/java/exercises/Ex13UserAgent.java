058de7d7d3746b067d7e295f7e4ccfe4ea98ad44
vivazz01 <crashdemo06@gmail.com>
2024-09-09 11:21:01 +0300

    ex13

---
+++ src/test/java/exercises/Ex13UserAgent.java
@@ -0,0 +1,28 @@
+package exercises;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class Ex13UserAgent {
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'",
+            "'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1'",
+            "'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'",
+            "'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0'",
+            "'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'"
+    })
+    public void ex13UserAgent(String userAgent) {
+        Response response = RestAssured
+                .given()
+                .queryParam(userAgent)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .andReturn();
+
+        String answer = response.asString();
+        System.out.println(answer);
+    }
+}
